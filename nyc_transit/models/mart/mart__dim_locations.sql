4fefa1879061908f33821248c5444814e447381c
Haroldo2097 <30965247+Haroldo2097@users.noreply.github.com>
2024-07-27 23:14:52 -0400

    corrected typos

--- nyc_transit/models/mart/mart__dim_locations.sql
+++ nyc_transit/models/mart/mart__dim_locations.sql
@@ -1,4 +1,3 @@
---This is just a fancy select *. Seclect all attributes out of taxi_zone_lookup
 select
-  {{ dbt_utils.star(ref('taxi_zone_lookup')) }}
-from {{ ref('taxi_zone_lookup') }}
+  {{ dbt_utils.star(ref('taxi+_zone_lookup')) }}
+from {{ref('taxi+_zone_lookup') }}
